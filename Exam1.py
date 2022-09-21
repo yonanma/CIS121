dcf7d487dbf1aecf9d10fad7b1d2a8eb0cb3ffd2
yonan.abraha@mnsu.edu <yonan.abraha@mnsu.edu>
2022-09-21 14:27:13 -0500

    exam 1

---
+++ Exam1.py
@@ -0,0 +1,32 @@
+#1 a: it does not work because it does not have a print statement
+#b:it will work but it ill be stuck in an infinte loop. i would fix it by adding one each time after the print statment
+#c it works
+#d it will not work beacause of the indentation
+#e it is never going to equal yo 5 so it can not break out of the loop and also it does not have a print satatment so it will not work
+#2
+"""
+milk = float(input("Enter amount of milk:"))#user input
+eggs = float(input("Enter amount of eggs:"))#user input
+bacon =  float(input("Enter amount of eggs:")) #user input
+
+x= (milk * 2.00 + eggs * 1.50 + bacon * 3.00) # calculating the price
+tax= x * 0.11# adding the tax
+
+print (x + tax)
+"""
+#3
+ph="6518083815"
+print("(",ph[0:3],")",ph[3:6],"-",ph[6:10]) 
+
+
+#4
+"""
+for x in range (1,61):
+    if 48 % x == 0:
+        if x < 15:
+            x % 2==0
+            print (x)
+        elif x >= 15:
+            x % 2==0
+            print("I generated the number",x, "randomly")
+"""
---
+++ week 5.py
@@ -0,0 +1,5 @@
+"""
+age = int(input("enter your age:"))
+
+for i in range (age,age+21):
+    print(i)"""
